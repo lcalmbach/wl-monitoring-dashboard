2b2c6e561c64d4ea9393cb8fd7c1eac54539e4f8
godot63 <lcalmbach@gmail.com>
2025-04-02 06:20:04 +0200

    add more stuff to pn

--- app/test.py
+++ app/test.py
@@ -1,8 +1,13 @@
 import panel as pn
 
+pn.extension("vega", "tabulator")
+
+pn.extension(sizing_mode="stretch_width", template="material")
+pn.config.raw_css.append("body { background: yellow; }")
+
 pn.extension()
 
-app = pn.Column('hello world v3')
+app = pn.Column('hello world v4')
 
 # Serveable entrypoint
 app.servable()
