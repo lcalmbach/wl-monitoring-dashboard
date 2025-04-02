1f6e959b52cc9e1323355364a0a87e9f7cf3e499
godot63 <lcalmbach@gmail.com>
2025-04-02 07:12:16 +0200

    one more try

--- app/dashboard.py
+++ app/dashboard.py
@@ -1,17 +1,14 @@
 
-import pandas as pd
 import panel as pn
-import altair as alt
-from groundwater import Groundwater
 
-pn.extension()
-
+pn.extension(sizing_mode="stretch_width")
+pn.extension(sizing_mode="stretch_width")
 
 component1 = pn.pane.Markdown("# Welcome to Component 1")
 component2 = pn.pane.Markdown("# Welcome to Component 2")
 component3 = pn.pane.Markdown("# Welcome to test 2")
 
-page = pn.template.FastListTemplate(
+page = pn.template.MaterialTemplate(
     title="Grundwassermonitoring Basel-Stadt",
     sidebar=[component1, component2],
     main=[component3]
