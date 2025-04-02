507e458baf9d85632bf54b4c8ec882c3246937e2
godot63 <lcalmbach@gmail.com>
2025-04-02 07:04:46 +0200

    reducce pn.extension

--- app/dashboard.py
+++ app/dashboard.py
@@ -4,14 +4,14 @@ import panel as pn
 import altair as alt
 from groundwater import Groundwater
 
-pn.extension("vega", "tabulator", sizing_mode="stretch_width")
+pn.extension()
 
 
 component1 = pn.pane.Markdown("# Welcome to Component 1")
 component2 = pn.pane.Markdown("# Welcome to Component 2")
 component3 = pn.pane.Markdown("# Welcome to test 2")
 
-page = pn.template.FastGridTemplate(
+page = pn.template.FastListTemplate(
     title="Grundwassermonitoring Basel-Stadt",
     sidebar=[component1, component2],
     main=[component3]
